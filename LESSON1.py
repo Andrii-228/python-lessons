ff4a111ca9529b1f57ccb524287d3d4e1ae8468f
Andrii-228 <andriipylat5@email.com>
2025-07-05 10:19:11 +0300

    first commit
    Signed-off-by: Andrii-228 <andriipylat5@email.com>

---
+++ LESSON1.py
@@ -0,0 +1,21 @@
+import telebot
+
+bot = telebot.TeleBot("7705300176:AAEA3XEYmY7TLxEA2fEKsavqIRiDLhAXYxo")
+name='andriibot'
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+  bot.reply_to(message, "Привіт! Як твої справи?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    if message.text in ['добре', 'чудово', 'нормально']:
+        bot.reply_to(message, 'Прекрасно, я радий це чути')
+    elif message.text == 'справи так собі':
+        bot.reply_to(message, 'Мені прикро, але сподіваюсь, що завтра справи будуть значно краще:)')
+    elif message.text == 'Як тебе звати?':
+        bot.reply_to(message, f'Мене звати {name}')    
+    else:
+        bot.reply_to(message, 'Вибач, я не запрограмований на цю відповідь')
+        
+    
+bot.infinity_polling()
